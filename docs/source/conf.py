22de7c041d0beb0866eaaf54e066391127af660d
Guillermo Cabezas <guicab@cartif.com>
2026-05-06 08:43:34 +0200

    added old md files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ release = '2026'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -60,3 +60,5 @@ html_theme_options = {
     "logo_only": False,
     "display_version": False,
 }
+
+
